6953afafbf4cedc91bdbc4093f33621d3bb42d1b
nguyenhaidang240800 <dang.nguyen@orientsoftware.com>
2022-07-28 18:00:19 +0700

    Update date FE for explore & wall

--- SocialNetworkWebApp-FE/src/app/dashboard/wall/wall.component.ts
+++ SocialNetworkWebApp-FE/src/app/dashboard/wall/wall.component.ts
@@ -12,4 +12,47 @@ export class WallComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public posts: any;
+
+  public currentUserProfile: any;
+
+  displayDate(date: Date) {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    const d = new Date(date);
+    const monthName = months[d.getMonth()];
+    const day = d.getDate() < 10 ? '0' + d.getDate() : d.getDate();
+    return monthName + ' ' + day;
+  }
+
+  editProfile() {
+    // this.dialog.open(EditprofileComponent);
+  }
+
+  displayModal(event: any) {
+    event.stopPropagation();
+    const wpContainer = document.querySelector('.wp-container');
+    // wpContainer.classList.remove('hidden');
+    // document.querySelector('body').style.overflowY = 'hidden';
+  }
+
+  closeModal(event: any) {
+    if (!event.target.closest('.wp-child')) {
+      const wpContainer = document.querySelector('.wp-container');
+      // wpContainer.classList.add('hidden');
+      // document.querySelector('body').style.overflowY = 'inherit';
+    }
+  }
 }
