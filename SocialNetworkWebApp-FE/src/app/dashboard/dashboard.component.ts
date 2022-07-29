588225d46171594fdbaea8f29da301f63c34a7a0
Đăng Nguyễn Hải <goingmyway243@gmail.com>
2022-07-30 02:07:19 +0700

    Big update dashboard FE

--- SocialNetworkWebApp-FE/src/app/dashboard/dashboard.component.ts
+++ SocialNetworkWebApp-FE/src/app/dashboard/dashboard.component.ts
@@ -11,6 +11,7 @@ export class DashboardComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.initElementsClickEvent();
   }
 
   navigateToWall(): void {
@@ -19,5 +20,26 @@ export class DashboardComponent implements OnInit {
 
   navigateToDashboard(): void {
     this.router.navigateByUrl('/home');
+  }
+
+  initElementsClickEvent(): void {
+    const bell = document.querySelector('.notification') as HTMLElement;
+    const popup = document.querySelector('.notification-popup') as HTMLElement;
+    const notiCount = document.querySelector('.notification-count') as HTMLElement;
+
+    bell.addEventListener('click', (event) => {
+      event.stopPropagation();
+      let visible = popup.style.display && popup.style.display !== 'none';
+      popup.style.display = visible ? 'none' : 'block';
+      notiCount.style.display = 'none';
+    });
+
+    popup.addEventListener('click', (event) => {
+      event.stopPropagation();
+    })
+
+    window.addEventListener('click', () => {
+      popup.style.display = 'none';
+    });
   }
 }
--- SocialNetworkWebApp-FE/src/app/dashboard/explore/explore.component.ts
+++ SocialNetworkWebApp-FE/src/app/dashboard/explore/explore.component.ts
@@ -10,6 +10,58 @@ export class ExploreComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.initElementsClickEvent();
   }
 
+  initElementsClickEvent(): void {
+    this.initMenuItemsClickEvent();
+    this.initSearchMessageEvent();
+    this.initThemeCustomization();
+  }
+
+  initMenuItemsClickEvent(): void {
+    const menuItems = document.querySelectorAll('.menu-item');
+
+    const changeActiveItem = () => {
+      menuItems.forEach(item => {
+        item.classList.remove('active');
+      });
+    };
+
+    menuItems.forEach(item => {
+      item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+      });
+    });
+  }
+
+  initSearchMessageEvent(): void {
+    const listMessage = document.querySelectorAll('.message');
+    const messageSearch = document.getElementById('message-search') as HTMLInputElement;
+
+    messageSearch?.addEventListener('keyup', () => {
+      const keyword = messageSearch.value.toLowerCase();
+      listMessage.forEach(chat => {
+        let name = chat.querySelector('h5')?.textContent?.toLowerCase();
+        const match = name && name.indexOf(keyword) != -1;
+        (chat as HTMLElement).style.display = match ? 'flex' : 'none';
+      })
+    });
+  }
+
+  initThemeCustomization(): void {
+    const themeBtn = document.getElementById('theme');
+    const themeModal = document.querySelector('.customize-theme') as HTMLElement;
+
+    themeBtn?.addEventListener('click', () => {
+      themeModal.style.display = 'grid';
+    });
+
+    themeModal.addEventListener('click', (event) => {
+      if ((event.target as HTMLElement).classList.contains('customize-theme')) {
+        themeModal.style.display = 'none';
+      }
+    })
+  }
 }
