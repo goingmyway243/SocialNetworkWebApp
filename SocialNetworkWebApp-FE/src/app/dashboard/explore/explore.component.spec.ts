bde66acdc19b6411b70ffccae3cdb41d6ffa970b
nguyenhaidang240800 <dang.nguyen@orientsoftware.com>
2022-07-29 17:40:48 +0700

    Update newfeeds FE

--- SocialNetworkWebApp-FE/src/app/dashboard/explore/explore.component.spec.ts
+++ SocialNetworkWebApp-FE/src/app/dashboard/explore/explore.component.spec.ts
@@ -8,9 +8,9 @@ describe('ExploreComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ExploreComponent ]
+      declarations: [ExploreComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ExploreComponent);
     component = fixture.componentInstance;
